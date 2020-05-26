906c2add3adfd272302c1c83d6c07876c10059b8
JC Yeh <jen.chieh.yeh@gmail.com>
2020-05-25 17:48:42 -0700

    add sendmail code

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,34 @@
+import smtplib
 from flask import Flask
 application = Flask(__name__)
 
+port = 587
+smtp_server = 'smtp-relay.sendinblue.com'
+username = 'jcyeh@familyfirstnetwork.org'
+password = '3fJCwD8sV1KAI2aW'
+
+from_add = 'jcyeh@familyfirstnetwork.org'
+to_add = 'jcyeh@larc.ee.nthu.edu.tw'
+
+sender = from_add
+receivers = [to_add]
+
+message = f"""\
+Subject: TEST MAIL 4
+To: {to_add}
+From: {from_add}
+
+This is test mail 4!"""
+
 @application.route("/")
 def hello():
-    return "Hello World, JC!"
+    smtpObj = smtplib.SMTP(smtp_server, port)
+    smtpObj.login(username, password)
+    #smtpObj.set_debuglevel(1)
+    smtpObj.sendmail(sender, receivers, message)
+    smtpObj.quit()
+
+    return "TEST Mail, JC!"
 
 if __name__ == "__main__":
     application.run()
